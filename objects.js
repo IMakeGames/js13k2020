9488b6d944d9e1e06795be7f933440413bc7dd8a
GaboJaras <gabojaras@gmail.com>
2020-08-28 13:05:20 -0400

    - added mouse controls - implemented initial muncher alg.

--- objects.js
+++ objects.js
@@ -44,12 +44,20 @@ function genPj(initX, initY){
         },
         update: function(){
             var t = this;
-            var yVar = 5*(((stateBits & 4) >> 2) - (stateBits & 1));
-            var xVar = 5*(((stateBits & 2) >> 1) - ((stateBits & 8) >> 3));
+            var xVar = 0;
+            var yVar = 0;
+            if(isMousePressed){
+                var xDiff = mousePos.x - t.hb.x ;
+                var yDiff = mousePos.y - t.hb.y ;
+                xVar = 5*(Math.abs(xDiff) > 200 ? 1*signUnit(xDiff) : xDiff/200);
+                yVar = 5*(Math.abs(yDiff) > 200 ? 1*signUnit(yDiff) : yDiff/200);
+            }
+            // var yVar = 5*(((stateBits & 4) >> 2) - (stateBits & 1));
+            // var xVar = 5*(((stateBits & 2) >> 1) - ((stateBits & 8) >> 3));
             t.hb.move(xVar, yVar);
             if(t.rope){
                 t.rope.attached = true;
-                var childDist = t.rope.getDist();
+                var childDist = getDist(t.hb, t.rope.hb);
                 var childForceX = (childDist.dist > 5 ? childDist.translateX : 0);
                 var childForceY = (childDist.dist > 5 ? childDist.translateY : 0);
                 t.hb.move(childForceX, childForceY);
@@ -61,14 +69,14 @@ function genPj(initX, initY){
             if(stateBits & 16){
                 stateBits ^= 16;
                 if(t.rope){
-                    if(soc = sockets.find(socket => Math.hypot(socket.conPt[0] - t.hb.x, socket.conPt[1] - t.hb.y) < 30 && !socket.rope)){
+                    if(soc = sockets.find(socket => getDist(t.hb,{x:socket.conPt[0],y:socket.conPt[1]}).dist < 30 && !socket.rope)){
                         soc.rope = t.rope;
                     }else{
                         t.rope.attached = false;
                     }
                     t.rope = null;
                 }else{
-                    t.rope = ropes.find(rope => Math.hypot(rope.hb.x - t.hb.x, rope.hb.y - t.hb.y) < 15) || null;
+                    t.rope = ropes.find(rope => getDist(t.hb, rope.hb).dist < 15) || null;
                 }
             }
             ctx.strokeStyle = 'blue';
@@ -77,6 +85,9 @@ function genPj(initX, initY){
     }
 }
 
+function signUnit(number){
+    return number/Math.abs(number);
+}
 function genRopeSec(initX, initY, amount){
     return {
         attached: false,
@@ -99,8 +110,12 @@ function genRopeSec(initX, initY, amount){
         },
         solve: function(bool){
             var t = this;
+            if(munch = munchers.find(muncher => muncher.state == "idle" && getDist(t.hb, muncher.hb).dist < 20)){
+                munch.food = t;
+                munch.state = "feed";
+            }
             if(t.child){
-                var dist = t.getDist();
+                var dist = getDist(t.hb,t.child.hb);
                 if(dist.dist > 8){
                     bool = false;
                     t.hb.x += dist.translateX;
@@ -114,19 +129,6 @@ function genRopeSec(initX, initY, amount){
                 t.hb.y = t.fixedY
                 return bool
             }
-        },
-        getDist: function() {
-            var t = this;
-            var xdiff = t.hb.x - t.child.hb.x;
-            var ydiff = t.hb.y - t.child.hb.y;
-            var dist = Math.hypot(xdiff, ydiff);
-            var scalDiff = (8 - dist) / dist;
-            scalDiff = scalDiff > 0 || isNaN(scalDiff) ? 0 : scalDiff;
-            return {
-                dist: dist,
-                translateX: xdiff * 0.5 * scalDiff,
-                translateY: ydiff * 0.5 * scalDiff,
-            };
         },
         draw: function(){
             var t = this;
@@ -169,7 +171,6 @@ function genSocket(x,y,type,dir,amount,link){
             var t = this;
             if(t.type == "end" && t.rope){
                 if(t.link && !t.link.rope){
-                    console.log("socket has link and not a rope. Generating rope");
                     t.link.rope = genRopeSec(t.link.conPt[0], t.link.conPt[1], t.rope.amount);
                     ropes.push(t.link.rope);
                 }
@@ -179,4 +180,53 @@ function genSocket(x,y,type,dir,amount,link){
             ctx.strokeRect(t.hb.x,t.hb.y,t.hb.w,t.hb.h);
         },
     }
+}
+
+function genMuncher(x,y){
+    return {
+        hb: genHb(x,y,30,30),
+        food: null,
+        state: "idle",
+        update: function(){
+            var t = this;
+            var playerDistance = getDist(t.hb,playa.hb);
+            switch(state){
+                case "idle":
+                    if(playerDistance.dist < 40){
+                        t.state = "attack";
+                    }
+                    break;
+                case "attack":
+                    t.hb.x += getDist.normalX*2;
+                    t.hb.y += getDist.normalY*2;
+                    if(playerDistance.dist > 60){
+                        t.state = "idle"
+                    }
+                    break;
+                case "feed":
+                    var foodDist = getDist(t.hb,t.food);
+                    if(foodDist.dist > 5){
+                        t.hb.x += foodDist.normalX;
+                        t.hb.y += foodDist.normalY;
+                    }else{
+                        //here the mf eats the shit
+                    }
+            }
+        }
+    }
+}
+
+function getDist(hb1, hb2) {
+    var xdiff = hb1.x - hb2.x;
+    var ydiff = hb1.y - hb2.y;
+    var dist = Math.hypot(xdiff, ydiff);
+    var scalDiff = (8 - dist) / dist;
+    scalDiff = scalDiff > 0 || isNaN(scalDiff) ? 0 : scalDiff;
+    return {
+        dist: dist,
+        translateX: xdiff * 0.5 * scalDiff,
+        translateY: ydiff * 0.5 * scalDiff,
+        normalX: xdiff/dist,
+        normalY: ydiff/dist
+    };
 }
